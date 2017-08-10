f703b0f0848f9b61f60bd7174cb9496b7a665ba2
Kelly Leahy <kelly.p.leahy@gmail.com>
2017-08-09 23:17:31 -0700

    try adding favicon stuff

--- main.go
+++ main.go
@@ -1,30 +1,37 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "log"
+    "net/http"
+    "os"
+    "io/ioutil"
+    "fmt"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+    port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-	router.Static("/images", "static/images")
-	router.Static("/css", "static/css")
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
+    router.Static("/images", "static/images")
+    router.Static("/css", "static/css")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+    files, err := ioutil.ReadDir("static/fav")
+    for _, file := range files {
+        router.Static(fmt.Sprintf("/%s", file.Name()), fmt.Sprintf("static/fav/%s", file.Name()))
+    }
 
-	router.Run(":" + port)
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
+
+    router.Run(":" + port)
 }
