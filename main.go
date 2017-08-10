75d0226509f0e478725c11b58b261ada07e76190
Adrian Withy <awithy@msn.com>
2017-08-09 17:00:03 -0700

    Trying to load static content

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ func main() {
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
+	router.Static("/images", "static/images")
+	router.Static("/css", "static/css")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
