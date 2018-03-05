ad87d46b992dedc2e1b4c0bf7c75ba2b0d312a7f
Matthew Parker <cadburylion@outlook.com>
2018-03-05 15:52:48 -0800

    Refactored index.html into react component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Landing from './components/landing';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Landing />
+      </div>
     );
   }
 }
