81d27a7b63ddf5b1088c74186c0fe8e175996cb9
Matthew Parker <cadburylion@outlook.com>
2018-03-06 09:40:49 -0800

    Imported blog files and fixed linter errors

---
+++ src/components/blog/article.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+
+const Article = ({article, time}) => {
+  article ? console.log('pubDate: ', article.pubDate) : ''
+  return (
+    <a href={article.link} target='_#' className='blog-wrapper'>
+      <div className='blog-image'>
+        {time}
+        <img src={article.thumbnail} alt='blog article thumbnail' />
+      </div>
+      <div className='article-title'>{article.title}</div>
+    </a>
+  )
+}
+
+export default Article
---
+++ src/components/blog/blog-field.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Article from './article.js'
+import TimeFormat from './time-format.js'
+import './style.scss'
+
+const BlogField = ({title, articles}) => {
+
+  const generateArticles = () => {
+    return articles.map((article, index) =>
+      <Article
+        key={article.pubDate + '-' + index}
+        article={article}
+        time={<TimeFormat time={article.pubDate}/>}
+      />
+    )
+  }
+
+  return (
+    <div className='blog-field'>
+      <h1 className='blog-title'>
+        {title}
+      </h1>
+      <div className='blog-content'>
+        {generateArticles()}
+      </div>
+    </div>
+  )
+}
+
+export default BlogField
